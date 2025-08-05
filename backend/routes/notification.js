6b9e9cdd1dc5fd2cc783e2521eaa580946761d10
Kartik Sharma <poojan4604@gmail.com>
2025-08-05 11:22:31 +0530

    Update notification.js
    adding the jwt

--- backend/routes/notification.js
+++ backend/routes/notification.js
@@ -1,7 +1,9 @@
 const notificationRouter = require('express').Router();
 const getNotifications = require('../controllers/getNotificationController');
 const createNotifications = require('../controllers/createNotificationController');
-
-notificationRouter.get('/', getNotifications);
+const verifyJwt=require('../middleware/verifyjwt');
+notificationRouter.get('/',verifyJwt(req,res)=>{
+  
+}, getNotifications);
 notificationRouter.post('/', createNotifications);
 module.exports = notificationRouter;
